20fce86f0b3ab08a5963c477f4aa7124639096d5
ikwan <innamulikwan15@gmail.com>
2022-06-22 18:18:14 +0700

    progres pertama

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>LOGIN MULTIUSER PHP</title>
+<link rel="stylesheet" type="text/css" href="desain.css">
+
+</head>
+<body>
+
+<h1>LOGIN </h1>
+ 
+<?php
+if(isset($_GET['pesan'])){
+if($_GET['pesan']=="gagal"){
+    echo "<div class='alert'>Username dan Password Salah !</div>";
+}
+}
+?>
+
+<div class="panel_login">
+<p class="tulisan_atas">Silahkan Masuk</p>
+
+<form action="cek_login.php" method="post">
+<label>Username</label>
+<input type="text" name="username" class="form_login" placeholder="Username" required="required">
+
+<label>Password</label>
+<input type="password" name="password" class="form_login" placeholder="Password" required="required">
+
+<input type="submit" class="tombol_login" value="LOGIN">
+
+<br/>
+<br/>
+
+</form>
+
+</div>
+
+
+</body>
+</html>
