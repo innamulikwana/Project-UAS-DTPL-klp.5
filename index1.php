8888005e0b72bb25422780b7f03e871210c5a055
Riyansaputra11 <107930539+Riyansaputra11@users.noreply.github.com>
2022-06-26 19:20:43 +0700

    progres tampilan awal
    uas dtpl

---
+++ index1.php
@@ -0,0 +1,136 @@
+<html>
+<head>
+<link rel="stylesheet"href="style4.css"text type="text/css">
+</head>
+<body>
+<marquee bgcolor ="yellow">selamat datang di Pondok Bakso Mas Brian </marquee>
+<nav class="menu"></div>
+    
+    <div class="logo"><a href=''>Pondok bakso mas brian</a></div>
+ 
+<ul>
+    <li><a href="#">beranda</a></li>
+    <li><a href="#">gallery</a>
+    <ul class="dropdown">
+        <li><a href="#">photo</a></li>
+        <li><a href="#">about</a></li>
+        <li><a href="#">lokasi</a></li>
+    </ul>
+    </li>
+    <li><a href="#">promotions</a></li>
+    <li><a href="index.php" >Sign up</a></li>
+                    
+</ul>
+</nav>
+
+
+<div class="wrapper">
+    <section id="home">
+        <img src="happy-male-chef-holding-ramen-noodle-cartoon-icon-illustration_138676-2460.jpg" width="500px"><br>
+        <div class="kolom">
+            <p class="deskripsi">Pondok Bakso Mas Brian</p>
+            <h2>jaga kesehatan tubuh</h2>
+            <p>enaknya bikin nendang, nikmatnya seharian</p>
+            <p> dapat di pesan via GOFOOD "pencet tombol di bawah"</p>
+            <p><a href="https://www.gojek.com/gofood/"class="tbl-pink">tombol</a></p>
+
+        </div>
+    </section>
+    <section id ="deskripsi">
+          <div class="kolom">
+             
+              <h2>Pondok Bakso Mas Brian</h2>
+              <p>Pndok bakso mas brian berdiri pada tahun 1550 pada awalnya Pondok bakso mas brian ini hanya memiliki satu kedai </p>
+                  <p>tetapi dengan pesatnya perkembangan zaman maka kami sekarang memilki banyak cabang</p>
+                  <p><a href="https://www.cnnindonesia.com/gaya-hidup/20160506024728-262-128930/menguak-asal-usul-mi-ayam" class="tbl-biru">pelajari lebih lanjut</a></p>
+
+          
+          </div>
+          <img src="noodle-restaurant-logo_54548-89.jpg" width="550px"><br>
+    </section>
+    <section id="makanan">
+        <div class="tengah">
+            <div class="kolom">
+                <p class="deskripsi">delicous</p>
+                <h2>Makanan</h2>
+                <p>memberikan cita rasa yang khas</p>
+
+            </div>
+            <div class="makanan-list">
+                <div class="kartu-makanan">
+                    <img src="download (3).jpg">
+                    <p>MIE AYAM</p>
+                    
+                </div>
+
+                <div class="kartu-makanan">
+                  <img src="download (1).jpg">
+                  <p>MIESO</p>
+              </div>
+              
+                   </div>
+        </div>
+    </section>
+    <section id="minuman">
+      <div class="tengah"  >
+          <div class="kolom">
+              <p class="deskripsi">delicous</p>
+              <h2>Minuman</h2>
+              <p>Kesegaran yang haqiqi dan menyejukan dahaga</p>
+
+      </div>
+      <div class="minuman-list">
+          <div class="kartu-minuman">
+              <img src="menu-drinkanddesserts-esjeruk.png">
+              <p>ice jeruk</p>
+          </div>
+          <div class="kartu-minuman">
+              <img src="es teh.png">
+              <p>ice teh</p>
+          </div>
+
+      </div>
+  </div> 
+    </section>
+    
+<div id="contact">
+    <div class="wrapper">
+        <div class="footer">
+            <div class="footer-section">
+
+              <h3>sosmed</h3>
+              <p>Instagram : Masbrianbakso</p>
+              <p>facebook : Masbrianbakso</p>
+
+            </div>
+            <div class="footer-section">
+
+              <h3>alamat</h3>
+              <p>jalan newyork city no10</p>
+              
+            </div>
+            <div class="footer-section">
+
+              <h3>no hp</h3>
+              <p>085675677678</p>
+              
+            </div>
+            <div class="footer-section">
+
+              <h3>email</h3>
+              <p>brianganteng@gmail.com</p>
+              
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="copyright">
+  <div class="wrapper">
+      &copy; 2021. <b>Pondok Bakso Mas Brian</b> All Rights Reserved
+  </div>
+</div>
+
+</body>
+</html>
+
